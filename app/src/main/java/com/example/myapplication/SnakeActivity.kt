ebef950f9245acb660e287988e128f6ed6ee20bc
VerdunJ <verdun.joris@gmail.com>
2021-03-08 16:18:49 +0100

    quelque changement

--- app/src/main/java/com/example/myapplication/SnakeActivity.kt
+++ app/src/main/java/com/example/myapplication/SnakeActivity.kt
@@ -23,12 +23,21 @@ class SnakeActivity : GLESHelperActivity(R.drawable.snake, 5) {
         tileSize = width / "Lancer".length
 
         println(" "+width+" "+height)
-        //glesHelper.renderStringCenteredxy("Lancer", width / 2, height /2, tileSize, true, false)
-        glesHelper.renderString("ance",1*tileSize,height /2,tileSize, 1)
-        glesHelper.renderString("L",0,height /2,tileSize, 14)
-        glesHelper.renderString("r",5*tileSize,height /2,tileSize, 2)
-        glesHelper.renderString(" ",5*tileSize,height /2 +tileSize,tileSize, 9)
-        glesHelper.renderStringCenteredxy("Snake", width / 2, tileSize, tileSize)
+        when(snake.menu) {
+            SnakeGame.Companion.SnakeGameMenu.NEW -> {
+                //glesHelper.renderStringCenteredxy("Lancer", width / 2, height /2, tileSize, true, false)
+                glesHelper.renderString("ance", 1 * tileSize, height / 2, tileSize, 1)
+                glesHelper.renderString("L", 0, height / 2, tileSize, 14)
+                glesHelper.renderString("r", 5 * tileSize, height / 2, tileSize, 2)
+                glesHelper.renderString(" ", 5 * tileSize, height / 2 + tileSize, tileSize, 9)
+                glesHelper.renderStringCenteredxy("Snake", width / 2, tileSize, tileSize)
+
+
+            } SnakeGame.Companion.SnakeGameMenu.INGAME -> {
+
+
+            }
+        }
       /*  when (snake.menu) {
             SnakeGame.Companion.SnakeGameMenu.NEW -> {
 
@@ -39,6 +48,18 @@ class SnakeActivity : GLESHelperActivity(R.drawable.snake, 5) {
     override fun onClick(v: View, e: MotionEvent, renderer: GLESHelperRenderer): Boolean {
         print(e.x.toInt())
         print(" "+e.y.toInt())
+        print("-------------------------------------------------------------------\n")
+        println(" "+(renderer.width-"Lancer".length/2))
+        println(" "+(renderer.height - tileSize * 3 / 2))
+        when (snake.menu) {
+            SnakeGame.Companion.SnakeGameMenu.NEW -> {
+                if (GLESUtils.isInRec(e.x.toInt(), e.y.toInt(),0,renderer.height /2,tileSize*"Lancer".length,tileSize))
+                    println("j'ai cliqué sur le bouton")
+                     snake.start()
+            }
+
+         }
         return true
+        }
+
     }
-}
--- app/src/main/java/com/example/myapplication/game/SnakeGame.kt
+++ app/src/main/java/com/example/myapplication/game/SnakeGame.kt
@@ -6,11 +6,35 @@ class SnakeGame: Game("Snake", R.id.snakeActivity) {
     var fini = false
     var gagne = false
     var menu= SnakeGame.Companion.SnakeGameMenu.NEW
+    lateinit var cells : Array<Array<SnakeCellState>>
+    var snake = listOf<SnakeBody>(SnakeBody(4, 8,14),SnakeBody(5, 8,1), SnakeBody(6, 8,4))
+
+    fun start() {
+        cells = Array(15) { Array(10) { SnakeGame.Companion.SnakeCellState() } }
+        menu = SnakeGame.Companion.SnakeGameMenu.INGAME
+        fini = false
+        gagne = false
+    }
 
     companion object{
         enum class SnakeGameMenu {
             INGAME,
             NEW
+        }
+
+        class SnakeCellState() {
+            var etat= SnakeState.VIDE
+            var sprite=9
+        }
+
+        enum class SnakeState {
+            VIDE,
+            SERPENT,
+            FRUIT
+        }
+
+        class SnakeBody(val x: Int, val y: Int, val imgNb: Int){
+
         }
     }
 }
